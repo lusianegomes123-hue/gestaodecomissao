7151b74b0868418370e2fd895369a06a224f4a98
Lusiane Gomes Simão <Lusianegomes123@gmail.com>
2026-03-05 06:55:41 -0300

    Revertendo Fallback SQLite para habilitar Supabase Postgres na nuvem

--- config.py
+++ config.py
@@ -2,18 +2,16 @@ import os
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    # Configuração do Banco de Dados: Forçando uso do SQLite Local devido ao vencimento do PostgreSQL no Render
+    
     database_url = os.getenv('DATABASE_URL')
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
-    
-    # ATENÇÃO: Override de resgate de emergência para usar o DB que foi upado no github
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'comissoes_prod.db')
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        'connect_args': {
-            'check_same_thread': False,
-            'timeout': 15
-        }
-    }
+        
+    if database_url:
+        SQLALCHEMY_DATABASE_URI = database_url
+    else:
+        # Fallback local SQLite file
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'comissoes_prod.db')
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'chave_secreta_padrao_desenvolvimento')
