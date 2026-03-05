8658286b85e70cf461597fb1269fe23dd920f664
Lusiane Gomes Simão <Lusianegomes123@gmail.com>
2026-03-04 21:23:22 -0300

    Override de emergencia: Forçando uso permanente do Banco de Dados SQLite local upado para contornar o vencimento do Banco Gratuito do Render

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ import os
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    # Configuração do Banco de Dados: Prioriza variável de ambiente (Render), senão usa SQLite local
+    # Configuração do Banco de Dados: Forçando uso do SQLite Local devido ao vencimento do PostgreSQL no Render
     database_url = os.getenv('DATABASE_URL')
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
     
-    SQLALCHEMY_DATABASE_URI = database_url or 'sqlite:///' + os.path.join(basedir, 'comissoes_prod.db')
+    # ATENÇÃO: Override de resgate de emergência para usar o DB que foi upado no github
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'comissoes_prod.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'chave_secreta_padrao_desenvolvimento')
